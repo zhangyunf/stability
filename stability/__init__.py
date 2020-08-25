b9222ef4cc28f89f49b5a1d860f5d9e4033c428e
zhangyunfei <245124085@qq.com>
2020-08-25 18:00:58 +0800

    添加网页

---
+++ stability/__init__.py
@@ -0,0 +1,13 @@
+"""
+一、文件描述
+setting.py:项目整体的配置文件
+urls.py:项目的路由配置文件
+wsgi.py:项目与WSGI协议兼容的web服务器入口
+manage.py:项目的管理文件
+二、基本命令
+1、创建项目
+创建项目 django-admin startproject 工程名称
+2、创建子程序
+1)、python3 manage.py startapp 子应用名称
+2)、
+"""
--- stability/urls.py
+++ stability/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from params import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index),
 ]
